b552a80f53e11993710b053d2d4d7671892693ef
Daniel <you@example.com>
2015-09-19 11:25:35 +0200

    arbete med states

---
+++ States/menu.js
@@ -0,0 +1,14 @@
+var menuState = {
+
+    create: function () {
+        var nameLabel = game.add.text(80, 80, 'Angry Sprites', {font: "50px Arial", fill: "#cc0000"});
+        var startLabel = game.add.text(80, game.world.height-80, 'press "space" to start', {font: "25px Arial", fill: "#bb0000"});
+        var startKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        startKey.onDown.addOnce(this.start, this);
+    },
+
+    start: function () {
+        game.state.start('play');
+    },
+
+}
---
+++ States/win.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Daniel on 2015-09-19.
+ */
